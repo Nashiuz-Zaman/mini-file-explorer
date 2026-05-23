ff185e01af11b04347ace435dbb7269715e11643
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2026-05-23 07:10:44 +0600

    feat(file system hook): create hook for handling file system

---
+++ src/hooks/useFileSystem.ts
@@ -0,0 +1,153 @@
+"use client";
+
+import { useEffect, useReducer, useState } from "react";
+
+import { IFileSystemState, TNode } from "@/types";
+import { ACTION_TYPES, fileSystemReducer } from "@/reducers/fileSystemReducer";
+
+// Empty Initial State
+const createEmptyInitialState = (): IFileSystemState => ({
+  rootId: "root",
+
+  nodes: {
+    root: {
+      id: "root",
+      name: "Desktop",
+      type: "folder",
+      parentId: null,
+      childrenIds: [],
+    },
+  },
+});
+
+// Lazy Initializer
+const initializeFileSystem = (): IFileSystemState => {
+  // SSR Safety
+  if (typeof window === "undefined") {
+    return createEmptyInitialState();
+  }
+
+  try {
+    const storedData = localStorage.getItem("mini_file_explorer_data");
+    if (storedData) {
+      return JSON.parse(storedData);
+    }
+
+    return createEmptyInitialState();
+  } catch (error) {
+    console.log("Failed to parse localStorage file system:", error);
+
+    return createEmptyInitialState();
+  }
+};
+
+export function useFileSystem() {
+  const [fileSystem, dispatch] = useReducer(
+    fileSystemReducer,
+    undefined,
+    initializeFileSystem,
+  );
+
+  // Navigation State
+  const [currentFolderId, setCurrentFolderId] = useState<string>("root");
+  const [activeFileId, setActiveFileId] = useState<string | null>(null);
+
+  // Persist State
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem(
+        "mini_file_explorer_data",
+        JSON.stringify(fileSystem),
+      );
+    }
+  }, [fileSystem]);
+
+  // Create Node
+  const createNode = (
+    parentId: string,
+    name: string,
+    type: "folder" | "text-file",
+  ) => {
+    const newId = crypto.randomUUID();
+
+    const newNode: TNode =
+      type === "folder"
+        ? {
+            id: newId,
+            name,
+            type: "folder",
+            parentId,
+            childrenIds: [],
+          }
+        : {
+            id: newId,
+            name,
+            type: "text-file",
+            parentId,
+            content: "",
+          };
+
+    dispatch({
+      type: ACTION_TYPES.create,
+      payload: {
+        parentId,
+        newNode,
+      },
+    });
+  };
+
+  // Rename Node
+  const renameNode = (id: string, newName: string) => {
+    dispatch({
+      type: ACTION_TYPES.rename,
+      payload: {
+        id,
+        newName,
+      },
+    });
+  };
+
+  // Delete Node
+  const deleteNode = (id: string) => {
+    dispatch({
+      type: ACTION_TYPES.delete,
+      payload: {
+        id,
+      },
+    });
+
+    // Reset invalid selections
+    if (activeFileId === id) {
+      setActiveFileId(null);
+    }
+
+    if (currentFolderId === id) {
+      setCurrentFolderId("root");
+    }
+  };
+
+  // Update File Content
+  const updateFileContent = (id: string, content: string) => {
+    dispatch({
+      type: ACTION_TYPES.updateContent,
+      payload: {
+        id,
+        content,
+      },
+    });
+  };
+
+  return {
+    fileSystem,
+    currentFolderId,
+    setCurrentFolderId,
+    activeFileId,
+    setActiveFileId,
+    createNode,
+    renameNode,
+    deleteNode,
+    updateFileContent,
+  };
+}
+
+export type TUseFileSystemReturn = ReturnType<typeof useFileSystem>;
